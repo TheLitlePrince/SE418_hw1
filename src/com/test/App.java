2e3e097620e46fba4e1735e0d22ed883a535b7e7
TheLitlePrince <yuanzhuo0118@gmail.com>
2019-03-06 13:05:37 +0800

    initial framework

---
+++ src/com/test/App.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("Beans.xml");
+        Hello obj = (Hello) context.getBean("hello");
+        obj.getMessage();
+    }
+}
